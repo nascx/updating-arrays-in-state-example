ae08ffd4bfd45bad73799e49e675e7646cf04607
Wesley Silva do Nascimento <wnascimento@MKMONDIAL.local>
2024-04-10 13:28:49 -0400

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,92 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
 
-export default function Home() {
+import React, { ChangeEvent, useState } from 'react';
+
+let i = 1
+
+type Tasks = {
+  id: number,
+  title: string,
+  status: string
+}
+
+
+const App = () => {
+
+  const initialTasks = [
+    {
+      id: 0,
+      title: 'Aprender inglês',
+      status: 'To do',
+    },
+    {
+      id: 1,
+      title: 'Aprender Python',
+      status: 'To do'
+    }
+  ]
+
+  const [title, setTitle] = useState<string>('')
+
+  const [tasks, setTasks] = useState<Tasks[]>(initialTasks)
+
+  //for add a new item in array
+  const add = () => {
+    setTasks(prevTasks => [
+      ...prevTasks, { id: i + 1, title: title, status: 'To do' }
+    ])
+    i + 1
+  }
+
+  //for remove item from array
+  const remove = (id: number) => {
+    setTasks(prevPlaylist =>
+      prevPlaylist.filter(muisc => muisc.id !== id)
+    )
+  }
+
+  //transforming a array
+  const tranforming = (id: number) => {
+    const newTasks = tasks.map((task) => {
+
+      if (task.id !== id) {
+        return task
+      } else {
+        return { ...task, status: 'Done' }
+      }
+    })
+    setTasks(newTasks)
+  }
+
+  //replacing items in an array
+  const replacing = (id: number) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id !== id) {
+        return task
+      } else {
+        return { id: task.id, title: 'replacing task', status: 'doing'}
+      }
+    })
+    setTasks(newTasks)
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <>
+      <h1>Add new music to playlist</h1>
+      <input type="text" value={title} onChange={(e: ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)} />
+      <button onClick={add}>Add</button>
+      <ul>
+        {tasks?.map((task) => (
+          <>
+            <li key={task.id}>{`${task.title}, status: ${task.status}`}</li>
+            <button onClick={() => remove(task.id)}>Delete</button>
+            <button onClick={() => tranforming(task.id)}>Done</button>
+            <button onClick={() => replacing(task.id)}>Replacing</button>
+          </>
+        ))}
+      </ul>
+    </>
   );
 }
+
+export default App;
